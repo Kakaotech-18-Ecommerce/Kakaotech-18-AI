85a63d4eb431eb84a651b7b24fb71b95f527dd35
Daehyun-Bigbread <bigdarkgold@gmail.com>
2024-08-16 13:45:12 +0900

    SCRUM-53 feature 당 weight 조정

--- recommender_system/model_training_and_prediction.py
+++ recommender_system/model_training_and_prediction.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 def train_and_predict(filtered_data):
     features = ['product_price', 'product_inventory', 'discount', 'product_quanity']
@@ -8,7 +9,16 @@ def train_and_predict(filtered_data):
 
     X = filtered_data[features]
     y = filtered_data[target]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # 특성에 가중치 적용
+    weights = [8.0, 1.5, 1.5, 0.8]  # 각 특성에 적용할 가중치('product_price', 'product_inventory', 'discount', 'product_quanity')
+    X_weighted = X * weights
+
+    # 특성 스케일링
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X_weighted)
+
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
     model = LinearRegression()
     model.fit(X_train, y_train)
@@ -16,6 +26,9 @@ def train_and_predict(filtered_data):
     y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
 
-    filtered_data['predicted_review_star'] = model.predict(filtered_data[features])
+    # 전체 데이터에 대한 추천 점수 계산
+    X_all_weighted = X * weights  # 전체 데이터에 가중치 적용
+    X_all_scaled = scaler.transform(X_all_weighted)  # 스케일링 적용
+    filtered_data['predicted_review_star'] = model.predict(X_all_scaled)
 
     return filtered_data, mse
